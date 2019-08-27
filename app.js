1ad90a86e191e353faedf6e350b5dd30214a1342
shaluks97 <46239857+shaluks97@users.noreply.github.com>
2019-08-27 11:16:41 +0530

    updated

--- app.js
+++ app.js
@@ -10,6 +10,25 @@ Mongoose.connect("mongodb+srv://V-13:vivek@cluster0-bgs7t.mongodb.net/studentdb1
 // Mongoose.connect("mongodb://localhost:27017/infodb");
 app.use(Express.static(__dirname+"/public"));
 
+// For CORS,Pgm Line no 12 to 29
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200' );
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 const InfoModel=Mongoose.model("infodetails",{
     name:String,
