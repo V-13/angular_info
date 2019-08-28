845fab7561edc95993929ebcdf979a8d325e84d6
shaluks97 <46239857+shaluks97@users.noreply.github.com>
2019-08-28 14:46:25 +0530

    update

--- app.js
+++ app.js
@@ -238,7 +238,7 @@ app.post('/userupdate',(req,res)=>{
     const Message=req.body.message;
     const Phone =req.body.phone;
     console.log(x);
-    var result = Infomodel.update({_id:x},{$set:{name:Name,email:Email,message:Message,phone:Phone}},(error,data)=>{
+    var result = InfoModel.update({_id:x},{$set:{name:Name,email:Email,message:Message,phone:Phone}},(error,data)=>{
         if(error){
             throw error;
             res.send(error);
